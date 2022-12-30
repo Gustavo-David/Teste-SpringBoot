6233658f008b01ce54cd7c9b391fbee544fc1f55
Gustavo Pinto <gustavodavidpinto@gmail.com>
2022-12-30 10:21:57 -0300

    Teste Order, Instant, ISO 8601

--- src/main/java/com/teste/demo/Entities/User.java
+++ src/main/java/com/teste/demo/Entities/User.java
@@ -1,11 +1,14 @@
 package com.teste.demo.Entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,8 +24,14 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
-    public User() {
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<Order>();
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public User() {
     }
 
     public User(Long id, String name, String email, String phone, String password) {
