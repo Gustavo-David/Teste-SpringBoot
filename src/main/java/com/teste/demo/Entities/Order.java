78bad0dffab0bf1b9e861c0e959695c1c5f11dc5
Gustavo Pinto <gustavodavidpinto@gmail.com>
2023-01-02 15:41:03 -0300

    Teste OrderStatus enum

--- src/main/java/com/teste/demo/Entities/Order.java
+++ src/main/java/com/teste/demo/Entities/Order.java
@@ -3,6 +3,8 @@ package com.teste.demo.Entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.teste.demo.Entities.Enum.OrderStatus;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,23 +17,25 @@ import jakarta.persistence.Table;
 @Table(name = "tb_order")
 public class Order implements Serializable {
     private static final long serialVersionUID = 1L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private Instant moment;
+    private Integer orderStatus;
 
     @ManyToOne
-    @JoinColumn(name ="client id")
+    @JoinColumn(name = "client id")
     private User client;
-
 
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -51,6 +55,16 @@ public class Order implements Serializable {
         this.moment = moment;
     }
 
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
     public User getUser() {
         return client;
     }
@@ -66,7 +80,6 @@ public class Order implements Serializable {
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
-
 
     @Override
     public boolean equals(Object obj) {
@@ -84,7 +97,5 @@ public class Order implements Serializable {
             return false;
         return true;
     }
-    
 
-    
 }
